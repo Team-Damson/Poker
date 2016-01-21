ce1a96cc4f35139f976afc579101e821110d27da
HristoVutov <vutov23@gmail.com>
2016-01-21 15:37:42 +0200

    .

---
+++ High-Quality-Code-Teamwork-Project-Poker/Poker/Dunno.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Poker
+{
+    public class Dunno
+    {
+        PlayerActions playerActions = new PlayerActions();
+
+        private static double RoundN(int sChips, int n)
+        {
+            double a = Math.Round((sChips / n) / 100d, 0) * 100;
+            return a;
+        }
+
+        public void HP(ref int sChips, ref bool sTurn, ref bool sFTurn, Label sStatus, double botPower, int n, int n1, int call,TextBox textboxPot, ref double Raise, ref bool raising)
+        {
+           
+            Random rand = new Random();
+            int rnd = rand.Next(1, 4);
+            if (call <= 0)
+            {
+                playerActions.Check(ref sTurn, sStatus, ref raising);
+            }
+            if (call > 0)
+            {
+                if (rnd == 1)
+                {
+                    if (call <= RoundN(sChips, n))
+                    {
+                        playerActions.Call(ref sChips, ref sTurn, sStatus, ref raising, ref call, textboxPot);
+                    }
+                    else
+                    {
+                        playerActions.Fold(ref sTurn, ref sFTurn, sStatus, ref raising);
+                    }
+                }
+                if (rnd == 2)
+                {
+                    if (call <= RoundN(sChips, n1))
+                    {
+                        playerActions.Call(ref sChips, ref sTurn, sStatus, ref raising, ref call, textboxPot);
+                    }
+                    else
+                    {
+                        playerActions.Fold(ref sTurn, ref sFTurn, sStatus, ref raising);
+                    }
+                }
+            }
+            if (rnd == 3)
+            {
+                if (Raise == 0)
+                {
+                    Raise = call * 2;
+                    playerActions.Raised(ref sChips, ref sTurn, sStatus, ref raising, ref Raise, ref call, textboxPot);
+                }
+                else
+                {
+                    if (Raise <= RoundN(sChips, n))
+                    {
+                        Raise = call * 2;
+                        playerActions.Raised(ref sChips, ref sTurn, sStatus, ref raising, ref Raise, ref call, textboxPot);
+                    }
+                    else
+                    {
+                        playerActions.Fold(ref sTurn, ref sFTurn, sStatus, ref raising);
+                    }
+                }
+            }
+            if (sChips <= 0)
+            {
+                sFTurn = true;
+            }
+        }
+        public void PH(ref int sChips, ref bool sTurn, ref bool sFTurn, Label sStatus, int n, int n1, int r, int call, TextBox textboxPot, ref double Raise, ref bool raising,double rounds)
+        {
+            Random rand = new Random();
+            int rnd = rand.Next(1, 3);
+            if (rounds < 2)
+            {
+                if (call <= 0)
+                {
+                    playerActions.Check(ref sTurn, sStatus, ref raising);
+                }
+                if (call > 0)
+                {
+                    if (call >= RoundN(sChips, n1))
+                    {
+
+                        playerActions.Fold(ref sTurn, ref sFTurn, sStatus, ref raising);
+                    }
+                    if (Raise > RoundN(sChips, n))
+                    {
+                        playerActions.Fold(ref sTurn, ref sFTurn, sStatus, ref raising);
+                    }
+                    if (!sFTurn)
+                    {
+                        if (call >= RoundN(sChips, n) && call <= RoundN(sChips, n1))
+                        {
+                            playerActions.Call(ref sChips, ref sTurn, sStatus, ref raising, ref call, textboxPot);
+                        }
+                        if (Raise <= RoundN(sChips, n) && Raise >= (RoundN(sChips, n)) / 2)
+                        {
+                            playerActions.Call(ref sChips, ref sTurn, sStatus, ref raising, ref call, textboxPot);
+                        }
+                        if (Raise <= (RoundN(sChips, n)) / 2)
+                        {
+                            if (Raise > 0)
+                            {
+                                Raise = RoundN(sChips, n);
+                                playerActions.Call(ref sChips, ref sTurn, sStatus, ref raising, ref call, textboxPot);
+                            }
+                            else
+                            {
+                                Raise = call * 2;
+                                playerActions.Call(ref sChips, ref sTurn, sStatus, ref raising, ref call, textboxPot);
+                            }
+                        }
+
+                    }
+                }
+            }
+            if (rounds >= 2)
+            {
+                if (call > 0)
+                {
+                    if (call >= RoundN(sChips, n1 - rnd))
+                    {
+                        playerActions.Fold(ref sTurn, ref sFTurn, sStatus, ref raising);
+                    }
+                    if (Raise > RoundN(sChips, n - rnd))
+                    {
+                        playerActions.Fold(ref sTurn, ref sFTurn, sStatus, ref raising);
+                    }
+                    if (!sFTurn)
+                    {
+                        if (call >= RoundN(sChips, n - rnd) && call <= RoundN(sChips, n1 - rnd))
+                        {
+                            playerActions.Call(ref sChips, ref sTurn, sStatus, ref raising, ref call, textboxPot);
+                        }
+                        if (Raise <= RoundN(sChips, n - rnd) && Raise >= (RoundN(sChips, n - rnd)) / 2)
+                        {
+                            playerActions.Call(ref sChips, ref sTurn, sStatus, ref raising, ref call, textboxPot);
+                        }
+                        if (Raise <= (RoundN(sChips, n - rnd)) / 2)
+                        {
+                            if (Raise > 0)
+                            {
+                                Raise = RoundN(sChips, n - rnd);
+                                playerActions.Raised(ref sChips, ref sTurn, sStatus, ref raising, ref Raise, ref call, textboxPot);
+                            }
+                            else
+                            {
+                                Raise = call * 2;
+                                playerActions.Raised(ref sChips, ref sTurn, sStatus, ref raising, ref Raise, ref call, textboxPot);
+                            }
+                        }
+                    }
+                }
+                if (call <= 0)
+                {
+                    Raise = RoundN(sChips, r - rnd);
+                    playerActions.Raised(ref sChips, ref sTurn, sStatus, ref raising, ref Raise, ref call, textboxPot);
+                }
+            }
+            if (sChips <= 0)
+            {
+                sFTurn = true;
+            }
+        }
+        public void Smooth(ref int botChips, ref bool botTurn, ref bool botFTurn, Label botStatus, int name, int n, int r, int call, TextBox textboxPot, ref double Raise, ref bool raising, ref double rounds)
+        {
+            Random rand = new Random();
+            int rnd = rand.Next(1, 3);
+            if (call <= 0)
+            {
+                playerActions.Check(ref botTurn, botStatus, ref raising);
+            }
+            else
+            {
+                if (call >= RoundN(botChips, n))
+                {
+                    if (botChips > call)
+                    {
+                        playerActions.Call(ref botChips, ref botTurn, botStatus, ref raising, ref call, textboxPot);
+                    }
+                    else if (botChips <= call)
+                    {
+                        raising = false;
+                        botTurn = false;
+                        botChips = 0;
+                        botStatus.Text = "Call " + botChips;
+                        textboxPot.Text = (int.Parse(textboxPot.Text) + botChips).ToString();
+                    }
+                }
+                else
+                {
+                    if (Raise > 0)
+                    {
+                        if (botChips >= Raise * 2)
+                        {
+                            Raise *= 2;
+                            playerActions.Raised(ref botChips, ref botTurn, botStatus, ref raising, ref Raise, ref call, textboxPot);
+
+                        }
+                        else
+                        {
+                            playerActions.Call(ref botChips, ref botTurn, botStatus, ref raising, ref call, textboxPot);
+                        }
+                    }
+                    else
+                    {
+                        Raise = call * 2;
+                        playerActions.Raised(ref botChips, ref botTurn, botStatus, ref raising, ref Raise, ref call, textboxPot);
+
+                    }
+                }
+            }
+            if (botChips <= 0)
+            {
+                botFTurn = true;
+            }
+        }
+    }
+}
---
+++ High-Quality-Code-Teamwork-Project-Poker/Poker/HandTypes.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Poker
+{
+    public class HandTypes
+    {
+        Dunno type = new Dunno();
+
+        public void HighCard(ref int sChips, ref bool sTurn, ref bool sFTurn, Label sStatus, double botPower, int call, TextBox textboxPot, ref double Raise, ref bool raising)
+        {
+            type.HP(ref sChips, ref sTurn, ref sFTurn, sStatus, botPower, 20, 25, call, textboxPot, ref Raise, ref raising);
+        }
+        public void PairTable(ref int sChips, ref bool sTurn, ref bool sFTurn, Label sStatus, double botPower, int call, TextBox textboxPot, ref double Raise, ref bool raising)
+        {
+            type.HP(ref sChips, ref sTurn, ref sFTurn, sStatus, botPower, 16, 25, call, textboxPot, ref Raise, ref raising);
+        }
+        public void PairHand(ref int sChips, ref bool sTurn, ref bool sFTurn, Label sStatus, double botPower, int call, TextBox textboxPot, ref double Raise, ref bool raising, double rounds)
+        {
+            Random rPair = new Random();
+            int rCall = rPair.Next(10, 16);
+            int rRaise = rPair.Next(10, 13);
+            if (botPower <= 199 && botPower >= 140)
+            {
+                type.PH(ref sChips, ref sTurn, ref sFTurn, sStatus, rCall, 6, rRaise, call, textboxPot, ref Raise, ref raising, rounds);
+            }
+            if (botPower <= 139 && botPower >= 128)
+            {
+                type.PH(ref sChips, ref sTurn, ref sFTurn, sStatus, rCall, 7, rRaise, call, textboxPot, ref Raise, ref raising, rounds);
+            }
+            if (botPower < 128 && botPower >= 101)
+            {
+                type.PH(ref sChips, ref sTurn, ref sFTurn, sStatus, rCall, 9, rRaise, call, textboxPot, ref Raise, ref raising, rounds);
+            }
+        }
+        public void TwoPair(ref int sChips, ref bool sTurn, ref bool sFTurn, Label sStatus, double botPower, int call, TextBox textboxPot, ref double Raise, ref bool raising, double rounds)
+        {
+            Random rPair = new Random();
+            int rCall = rPair.Next(6, 11);
+            int rRaise = rPair.Next(6, 11);
+            if (botPower <= 290 && botPower >= 246)
+            {
+                type.PH(ref sChips, ref sTurn, ref sFTurn, sStatus, rCall, 3, rRaise, call, textboxPot, ref Raise, ref raising, rounds);
+            }
+            if (botPower <= 244 && botPower >= 234)
+            {
+                type.PH(ref sChips, ref sTurn, ref sFTurn, sStatus, rCall, 4, rRaise, call, textboxPot, ref Raise, ref raising, rounds);
+            }
+            if (botPower < 234 && botPower >= 201)
+            {
+                type.PH(ref sChips, ref sTurn, ref sFTurn, sStatus, rCall, 4, rRaise, call, textboxPot, ref Raise, ref raising, rounds);
+            }
+        }
+        public void ThreeOfAKind(ref int sChips, ref bool sTurn, ref bool sFTurn, Label sStatus, int name, double botPower, int call, TextBox textboxPot, ref double Raise, ref bool raising, ref double rounds)
+        {
+            Random tk = new Random();
+            int tCall = tk.Next(3, 7);
+            int tRaise = tk.Next(4, 8);
+            if (botPower <= 390 && botPower >= 330)
+            {
+                type.Smooth(ref sChips, ref sTurn, ref sFTurn, sStatus, name, tCall, tRaise, call, textboxPot, ref Raise, ref raising, ref rounds);
+            }
+            if (botPower <= 327 && botPower >= 321)//10  8
+            {
+                type.Smooth(ref sChips, ref sTurn, ref sFTurn, sStatus, name, tCall, tRaise, call, textboxPot, ref Raise, ref raising, ref rounds);
+            }
+            if (botPower < 321 && botPower >= 303)//7 2
+            {
+                type.Smooth(ref sChips, ref sTurn, ref sFTurn, sStatus, name, tCall, tRaise, call, textboxPot, ref Raise, ref raising, ref rounds);
+            }
+        }
+        public void Straight(ref int sChips, ref bool sTurn, ref bool sFTurn, Label sStatus, int name, double botPower, int call, TextBox textboxPot, ref double Raise, ref bool raising, ref double rounds)
+        {
+            Random str = new Random();
+            int sCall = str.Next(3, 6);
+            int sRaise = str.Next(3, 8);
+            if (botPower <= 480 && botPower >= 410)
+            {
+                type.Smooth(ref sChips, ref sTurn, ref sFTurn, sStatus, name, sCall, sRaise, call, textboxPot, ref Raise, ref raising, ref rounds);
+            }
+            if (botPower <= 409 && botPower >= 407)//10  8
+            {
+                type.Smooth(ref sChips, ref sTurn, ref sFTurn, sStatus, name, sCall, sRaise, call, textboxPot, ref Raise, ref raising, ref rounds);
+            }
+            if (botPower < 407 && botPower >= 404)
+            {
+                type.Smooth(ref sChips, ref sTurn, ref sFTurn, sStatus, name, sCall, sRaise, call, textboxPot, ref Raise, ref raising, ref rounds);
+            }
+        }
+        public void Flush(ref int sChips, ref bool sTurn, ref bool sFTurn, Label sStatus, int name, double botPower, int call, TextBox textboxPot, ref double Raise, ref bool raising, ref double rounds)
+        {
+            Random fsh = new Random();
+            int fCall = fsh.Next(2, 6);
+            int fRaise = fsh.Next(3, 7);
+            type.Smooth(ref sChips, ref sTurn, ref sFTurn, sStatus, name, fCall, fRaise, call, textboxPot, ref Raise, ref raising, ref rounds);
+        }
+        public void FullHouse(ref int sChips, ref bool sTurn, ref bool sFTurn, Label sStatus, int name, double botPower, int call, TextBox textboxPot, ref double Raise, ref bool raising, ref double rounds)
+        {
+            Random flh = new Random();
+            int fhCall = flh.Next(1, 5);
+            int fhRaise = flh.Next(2, 6);
+            if (botPower <= 626 && botPower >= 620)
+            {
+                type.Smooth(ref sChips, ref sTurn, ref sFTurn, sStatus, name, fhCall, fhRaise, call, textboxPot, ref Raise, ref raising, ref rounds);
+            }
+            if (botPower < 620 && botPower >= 602)
+            {
+                type.Smooth(ref sChips, ref sTurn, ref sFTurn, sStatus, name, fhCall, fhRaise, call, textboxPot, ref Raise, ref raising, ref rounds);
+            }
+        }
+        public void FourOfAKind(ref int sChips, ref bool sTurn, ref bool sFTurn, Label sStatus, int name, double botPower, int call, TextBox textboxPot, ref double Raise, ref bool raising, ref double rounds)
+        {
+            Random fk = new Random();
+            int fkCall = fk.Next(1, 4);
+            int fkRaise = fk.Next(2, 5);
+            if (botPower <= 752 && botPower >= 704)
+            {
+                type.Smooth(ref sChips, ref sTurn, ref sFTurn, sStatus, name, fkCall, fkRaise, call, textboxPot, ref Raise, ref raising, ref rounds);
+            }
+        }
+        public void StraightFlush(ref int sChips, ref bool sTurn, ref bool sFTurn, Label sStatus, int name, double botPower, int call, TextBox textboxPot, ref double Raise, ref bool raising, ref double rounds)
+        {
+            Random sf = new Random();
+            int sfCall = sf.Next(1, 3);
+            int sfRaise = sf.Next(1, 3);
+            if (botPower <= 913 && botPower >= 804)
+            {
+                type.Smooth(ref sChips, ref sTurn, ref sFTurn, sStatus, name, sfCall, sfRaise, call, textboxPot, ref Raise, ref raising, ref rounds);
+            }
+        }
+    }
+}
