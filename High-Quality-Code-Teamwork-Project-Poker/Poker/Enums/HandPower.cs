5dd35d9e25f4588507fcd2813c39d7abaa7b8710
Daniela Petrova <daniella_petrova@abv.bg>
2016-01-24 17:31:52 +0200

    intrudused HandPower and refactored CheckHandType

--- High-Quality-Code-Teamwork-Project-Poker/Poker/Enums/HandPower.cs
+++ High-Quality-Code-Teamwork-Project-Poker/Poker/Enums/HandPower.cs
@@ -1,22 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Poker.Enums
+﻿namespace Poker.Enums
 {
-    public enum HandPower
+    public struct HandPower
     {
-        HighCard,
-        OnePair,
-        TwoPairs,
-        ThreeOfAKind,
-        Straigth,
-        Flush,
-        FullHouse,
-        FourOfAKind,
-        StraightFlush,
-        RoyalFlush
+        public const double HighCard = -1;
+
+        public const double Undefined = 0;
+
+        public const double PairFromHand = 1;
+
+        public const double Pair = 2;
+
+        public const double ThreeOfAKind = 3;
+
+        public const double Straigth = 4;
+
+        public const double Flush = 5;
+
+        public const double FlushWithAce = 5.5;
+
+        public const double FullHouse = 6;
+
+        public const double FourOfAKind = 7;
+
+        public const double StraightFlush = 8;
+
+        public const double RoyalFlush = 9;
     }
 }
